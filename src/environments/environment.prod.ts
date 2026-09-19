7ed36781a688e599a9c957faa55d5a4cf4db1d52
Ashwin Sharma <mohit200914gmail.com>
2026-09-19 13:33:00 +0530

    changed Azure Production URL in Environmen_Production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
  */
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api-app-service.azurewebsites.net/api',
+  apiUrl: 'https://dmellobuisnessportal-gtf0fscqh8grbwdb.westus3-01.azurewebsites.net/api',
   apiTimeout: 30000,
   logging: {
     enableLogging: false,

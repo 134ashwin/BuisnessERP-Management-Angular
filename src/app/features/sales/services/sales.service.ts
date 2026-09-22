e7a24dbefdf5d4e737e7834e521015606db761a8
Ashwin Sharma <mohit200914gmail.com>
2026-09-22 22:47:50 +0530

    test1

--- src/app/features/sales/services/sales.service.ts
+++ src/app/features/sales/services/sales.service.ts
@@ -6,7 +6,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SalesOrder, PagedResult, SalesFilter } from '../models/sales.model';
-import { environment } from '../../../../environments/environment.prod'; // Import environment for baseUrl
+import { environment } from '../../../../environments/environment'; // Import environment for baseUrl
 
 
 @Injectable({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
  */
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://localhost:7098/api/', //http://localhost:3000/api old working Url.
   apiTimeout: 30000,
   logging: {
     enableLogging: true,
